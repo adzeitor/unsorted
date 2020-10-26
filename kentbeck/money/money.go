07fd9a598e31c9c31542b849e043c2146bd1327d
Alexander Voykov <a.voykov@omprussia.ru>
2020-10-26 23:09:08 +0300

    Kent beck TDD money 2: side effects

--- kentbeck/money/money.go
+++ kentbeck/money/money.go
@@ -4,8 +4,8 @@ type DollarValue struct {
 	Amount int
 }
 
-func (dollar *DollarValue) Times(n int) {
-	dollar.Amount *= n
+func (dollar DollarValue) Times(n int) DollarValue {
+	return Dollar(dollar.Amount * n)
 }
 
 func Dollar(amount int) DollarValue {
--- kentbeck/money/money_test.go
+++ kentbeck/money/money_test.go
@@ -6,6 +6,10 @@ import (
 
 func TestDollar_Times(t *testing.T) {
 	five := Dollar(5)
-	five.Times(2)
-	assert(t, 10, five.Amount)
+
+	result := five.Times(2)
+	assert(t, 10, result.Amount)
+
+	result = five.Times(3)
+	assert(t, 15, result.Amount)
 }
